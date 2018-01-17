18415bd0b6a6e6d766f358d38aa34f71a6ba04c1
wangjiang <wangjiang@zhongchebaolian.com>
2018-01-17 16:49:19 +0800

    测试pod

---
+++ TestPodSDK.framework/Headers/TestPodSDK.h
@@ -0,0 +1,19 @@
+//
+//  TestPodSDK.h
+//  TestPodSDK
+//
+//  Created by Cheer_Harry on 2018/1/17.
+//  Copyright © 2018年 Cheer_Harry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestPodSDK.
+FOUNDATION_EXPORT double TestPodSDKVersionNumber;
+
+//! Project version string for TestPodSDK.
+FOUNDATION_EXPORT const unsigned char TestPodSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestPodSDK/PublicHeader.h>
+#import <TestPodSDK/TestPodSDKTools.h>
+
---
+++ TestPodSDK.framework/Headers/TestPodSDKTools.h
@@ -0,0 +1,16 @@
+//
+//  TestPodSDKTools.h
+//  TestPodSDK
+//
+//  Created by Cheer_Harry on 2018/1/17.
+//  Copyright © 2018年 Cheer_Harry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestPodSDKTools : NSObject
+
+- (void)TestPodSDKNSNog;
+
+
+@end
